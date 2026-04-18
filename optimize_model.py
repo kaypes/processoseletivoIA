65d2090922d0f2aaeb997409c24b070e0d62f35c
Kayque Pereira <114672278+kaypes@users.noreply.github.com>
2026-04-18 15:01:19 -0300

    perf: criar utilitario de conversao tflite

--- optimize_model.py
+++ optimize_model.py
@@ -1,4 +1,12 @@
 import tensorflow as tf
 import os
 
-#insira seu código aqui
+model = tf.keras.models.load_model("model.h5")
+print("Modelo carregado: model.h5")
+
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
+converter.optimizations = [tf.lite.Optimize.DEFAULT]
+tflite_model = converter.convert()
+
+with open("model.tflite", "wb") as f:
+    f.write(tflite_model)
